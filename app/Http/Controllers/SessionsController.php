af18ca764e8e0b26602565210976c336fc91701b
康安喜 <kanganxi@gmail.com>
2021-03-15 13:23:20 +0800

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -24,6 +24,7 @@ class SessionsController extends Controller
     public function store(Request $request)
     {
         $credentials = $this->validate($request,[
+
             'email'=>'required|email|max:255',
             'password' => 'required'
         ]);
@@ -31,9 +32,17 @@ class SessionsController extends Controller
         if(Auth::attempt($credentials,$request->has('remember')))
         {
             //登录成功后的相关操作
-            session()->flash('success','欢迎回来');
-            $fallback = route('users.show',Auth::user());
-            return redirect()->intended($fallback);
+            if(Auth::user()->activated)
+            {
+                session()->flash('success','欢迎回来');
+                $fallback = route('users.show',Auth::user());
+                return redirect()->intended($fallback);
+            }else{
+                Auth::logout();
+                session()->flash('warning','你的账户未激活，请检查邮箱中的注册邮件进行激活');
+                return redirect('/');
+            }
+
         } else {
             //登录失败后的相关操作
             session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
